84a3a05d0dcbb6485d97cdee8eee0c0dd3d42b90
Jaime Toba <jhtobav@gmail.com>
2020-05-10 12:42:37 -0500

    Creating notes, new template and view for such purpose

--- core/notes/urls.py
+++ core/notes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'notes'
 urlpatterns = [
     # ex: /notes/
     path('', views.notes, name='notes'),
-    # ex: /notes/1
+    # ex: /notes/1/
     path('<int:note_id>/', views.note_details, name='note_details'),
+    # ex: /notes/create/
+    path('create/', views.note_create, name='note_create'),
 ]
--- core/notes/views.py
+++ core/notes/views.py
@@ -1,5 +1,7 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.utils import timezone
+from django.urls import reverse
 
 from .models import Note
 
@@ -18,3 +20,10 @@ def note_details(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, 'notes/note_details.html', {'note': note})
 
+
+def note_create(request):
+    note = Note(note_title=request.POST['note_title'],
+                note_content=request.POST['note_content'],
+                note_last_updated_time=timezone.now())
+    note.save()
+    return HttpResponseRedirect(reverse('notes:notes'))
