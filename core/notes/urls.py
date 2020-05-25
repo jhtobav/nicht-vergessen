07f411990a9910f1283a6c96853edd681f9b4e80
Jaime Toba <jhtobav@gmail.com>
2020-05-25 18:43:34 -0500

    added field validations in html

--- core/notes/urls.py
+++ core/notes/urls.py
@@ -6,7 +6,6 @@ app_name = 'notes'
 urlpatterns = [
     # ex: /notes/
     path('', views.notes, name='notes'),
-    path('$', views.notes, name='notes'),
     # ex: /notes/1/
     path('<int:note_id>/', views.note_details, name='note_details'),
     # ex: /notes/create/
